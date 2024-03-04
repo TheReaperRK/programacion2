0ec32c9936777d6e0b5b64c6e927e2ad21b006be
TheReaperRK <carlosmendoza2003@gmail.com>
2024-03-04 10:13:58 +0100

    cercador de inputs a la paraula

--- penjat/penjat.java
+++ penjat/penjat.java
@@ -1,8 +1,11 @@
 package penjat;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class penjat {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -36,16 +39,45 @@ public class penjat {
                             {"/"," "," "," "," ","\\"," "," "}};
 
         mostrarTaulell(taulell);
-        
         String paraula = generarParaula(paraules);
         char[] paraulaSeparada = new char[paraula.length()];
 
-        mostrarParaula(paraulaSeparada,paraula);
+        codificarParaula(paraulaSeparada,paraula);
+
+        while (true){
+            String input = sc.nextLine();
+
+            int resposta = comprovarLletra(input, paraulaSeparada, paraula);
+            if (resposta == 0){
+                System.out.println("S'ha trobat la lletra " + input.charAt(0));
+                mostrarParaula(paraulaSeparada);
+            }
+        }
+    }
+
+    static void mostrarParaula(char[] paraulaSeparada){
+        for (char lletra : paraulaSeparada){
+            System.out.print(lletra + " ");
+        }
     }
 
     static String generarParaula(String[] paraules){
         int random = (int)(Math.random()*((paraules.length+1)));
         return paraules[random];
+    }
+
+    static int comprovarLletra(String input, char[] paraulaSeparada, String paraula){
+        boolean trobada = false;
+        for (int  i = 0; i < paraula.length(); i++){
+            if (input.charAt(0) == paraula.charAt(i)){
+                paraulaSeparada[i] = input.charAt(0);
+                trobada = true;
+            }
+        }
+        if (trobada)
+            return 0;
+        else
+            return-1;
     }
     
     static void mostrarTaulell(String[][] taulell){
@@ -57,7 +89,7 @@ public class penjat {
         }
     }
 
-    static void mostrarParaula(char[] paraulaSeparada, String paraula){
+    static void codificarParaula(char[] paraulaSeparada, String paraula){
         System.out.println(paraula);
         for (int i = 0; i < paraula.length(); i++){
             paraulaSeparada[i] = '*';
