25acf539ce0a0ed6ec2934e6d0ecb7143b7fb124
TheReaperRK <carlosmendoza2003@gmail.com>
2024-03-04 09:13:19 +0100

    seleccionar palabras

--- penjat/penjat.java
+++ penjat/penjat.java
@@ -17,14 +17,35 @@ public class penjat {
                             "pissarra","professor","quadrat","taronja",
                             "tramvia","trapezi","tricicle","violeta"};
 
-        String[][] taulell = {{" "," ","_","_","_","_","_"," "},
+        String[][] taulellFinal =
+                            {{" "," ","_","_","_","_","_"," "},
                             {" "," ","|"," "," "," O"," "," "},
                             {" "," ","|"," "," /","|","\\"," "},
                             {" "," ","|"," "," /"," ","\\"," "},
                             {" "," ","|"," "," "," "," "," "},
                             {" ","_","|","_","_"," "," "," "},
                             {"/"," "," "," "," ","\\"," "," "}};
+
+        String[][] taulell =
+                            {{" "," ","_","_","_","_","_"," "},
+                            {" "," ","|"," "," "," "," "," "},
+                            {" "," ","|"," "," "," "," "," "},
+                            {" "," ","|"," "," "," "," "," "},
+                            {" "," ","|"," "," "," "," "," "},
+                            {" ","_","|","_","_"," "," "," "},
+                            {"/"," "," "," "," ","\\"," "," "}};
+
         mostrarTaulell(taulell);
+        
+        String paraula = generarParaula(paraules);
+        char[] paraulaSeparada = new char[paraula.length()];
+
+        mostrarParaula(paraulaSeparada,paraula);
+    }
+
+    static String generarParaula(String[] paraules){
+        int random = (int)(Math.random()*((paraules.length+1)));
+        return paraules[random];
     }
     
     static void mostrarTaulell(String[][] taulell){
@@ -33,6 +54,14 @@ public class penjat {
                 System.out.print(valor);
             }
             System.out.println();
+        }
+    }
+
+    static void mostrarParaula(char[] paraulaSeparada, String paraula){
+        System.out.println(paraula);
+        for (int i = 0; i < paraula.length(); i++){
+            paraulaSeparada[i] = '*';
+            System.out.print(paraulaSeparada[i] + " ");
         }
     }
     
